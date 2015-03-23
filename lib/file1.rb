22af829dca40ce98537b724a50826a4c1f78bcbf
Rob Esris <resris@asheavenue.com>
2015-03-22 22:32:26 -0400

    Some new changes

--- lib/file1.rb
+++ lib/file1.rb
@@ -1,12 +1,21 @@
-class Television                              # TOTAL COMPLEXITY: 10
+class Television                              # TOTAL COMPLEXITY: 6 + 1 == 7
+  # Complexity got better!
   def watch(a, b, c=5)                        # 1
     something = (2.0000001 ^ 3)               # 2
     something += c * 2                        # 3
-    @something_else = a - b / something * 8   # 4
+    help_me_out
     something
   end
 
-  def foo                                     # TOTAL COMPLEXITY: 3
+  def help_me_out                             # New method - 4 complexity
+    @something_else = a - b / something * 8   # 4
+  end
+
+  # Complexity got worse!
+  def foo                                     # TOTAL COMPLEXITY: 14 + 1 == 15
     @something_else == 1 + 3.005              # 3
+    [1, 2, 3, 4, 6, 5, 10.0].each do |n|      # 7
+      puts n + 1 + 100.0                      # 4
+    end
   end
 end
--- lib/file2.rb
+++ lib/file2.rb
@@ -1,6 +1,6 @@
 require 'file1'
 
-def foo(args)                               # TOTAL COMPLEXITY: 17
+def foo(args)                               # TOTAL COMPLEXITY: 17 + 1 == 18
                                             # (method name shouldn't confict with foo in other file)
   if args[1] == args[2]                     #  2
     args[3] = 5 * 8 * 3 * args[5] / args[6] # 10
